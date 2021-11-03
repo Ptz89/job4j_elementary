1377e1b6c6bcaa033fc58bd25e4c03fae3ca9054
Ptz_89 <ptz_89@mail.ru>
2021-11-03 13:19:45 +0300

    5.7. Ипотека [#156315 #223683]

--- src/main/java/ru/job4j/loop/Mortgage.java
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -3,18 +3,11 @@ package ru.job4j.loop;
 public class Mortgage {
     public static int year(double amount, int salary, double percent) {
         int year = 0;
-        double dolg = amount + (amount * percent / 100);
-        while (dolg > salary) {
+        while (amount > 0) {
+            amount += amount * (percent / 100) - salary;
             year++;
-            dolg -= salary;
-            dolg = dolg + (dolg * percent / 100);
         }
-        year += 1;
+
         return year;
-    }
-
-    public static void main(String[] args) {
-        System.out.println(Mortgage.year(100, 120, 50));
-
     }
 }
